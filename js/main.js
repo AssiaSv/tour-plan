aaff97512931f480197397d2b33d4ccbbbc68dc6
AssiaSv <svornikova2013@yandex.ru>
2021-11-26 16:09:21 +0300

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   
   loop: true,
@@ -6,8 +6,26 @@ var mySwiper = new Swiper('.swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+  
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
+});
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
   
   keyboard: {
