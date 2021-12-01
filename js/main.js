aa52e3b30865ddd142ec3de9b0a846f320f215cb
AssiaSv <svornikova2013@yandex.ru>
2021-12-02 02:36:50 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -40,4 +40,11 @@ $(".newsletter").parallax({
   speed: 0.4,
 });
 
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
 
